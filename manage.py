0e57a451b0fbb6a110b9b00f24b8e7b32387944b
albertoruz23 <albertoruz23@gmail.com>
2020-03-30 00:14:18 -0500

    Add files via upload

--- manage.py
+++ manage.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
+
 import os
 import sys
-
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'test1.settings')
@@ -12,7 +12,7 @@ def main():
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
+            "forget to activate a virtual environment1?"
         ) from exc
     execute_from_command_line(sys.argv)
 
